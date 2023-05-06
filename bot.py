abfc57f0ba8880689d7a09133312cfdf9729d8fa
MalithCrpt <81417838+MalithCrpt@users.noreply.github.com>
2023-05-06 15:07:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,159 +6,162 @@ from pyrogram.types import Message
 import os
 
 from config import Config
- 
-bot_token=Config.TG_BOT_TOKEN
-api_id=Config.APP_ID
-api_hash=Config.API_HASH
 
-app = Client("Malith:memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+bot_token = Config.TG_BOT_TOKEN
+api_id = Config.APP_ID
+api_hash = Config.API_HASH
 
-  # sending files
+app = Client("Malith:memory:",
+             api_id=api_id,
+             api_hash=api_hash,
+             bot_token=bot_token)
+
+
+# sending files
 async def sender(msg, download_location, fileName):
-    await msg.reply_document(download_location, caption=f"{fileName}",quote=True)
-    os.remove(download_location)
- 
+  await msg.reply_document(download_location,
+                           caption=f"{fileName}",
+                           quote=True)
+  os.remove(download_location)
+
+
 # downloading link
 async def download(msg, download_location, url, formatt):
-    try:
-        command_to_exec = [
-            "youtube-dl",
-            "-f",
-            formatt,
-            "-o",
-            download_location,
-            url
-        ]
-        await msg.reply_text("Downloading!!", quote=True)
-        # logger.info(command_to_exec)
-        process = await asyncio.create_subprocess_exec(
-            *command_to_exec,
-            # stdout must a pipe to be accessible as process.stdout
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        # Wait for the subprocess to finish
-        await process.communicate()
-    except:
-        await msg.reply_text("An err occured!!", quote=True)
- 
- 
+  try:
+    command_to_exec = [
+      "youtube-dl", "-f", formatt, "-o", download_location, url
+    ]
+    await msg.reply_text("Downloading!!", quote=True)
+    # logger.info(command_to_exec)
+    print(command_to_exec)
+    process = await asyncio.create_subprocess_exec(
+      *command_to_exec,
+      # stdout must a pipe to be accessible as process.stdout
+      stdout=asyncio.subprocess.PIPE,
+      stderr=asyncio.subprocess.PIPE,
+    )
+    # Wait for the subprocess to finish
+    await process.communicate()
+    if process.returncode == 0:
+      await sender(msg=msg,
+                   download_location=download_location,
+                   fileName=download_location)
+    print(process.returncode)
+  except:
+    await msg.reply_text("An err occured!!", quote=True)
+
+
 # getting forrmat details
 async def getting_filename(url, msg):
-    command_to_exec = [
-            "youtube-dl",
-            "--no-warnings",
-            "--youtube-skip-dash-manifest",
-            "-j",
-            url
-        ]
-        # logger.info(command_to_exec)
-    process = await asyncio.create_subprocess_exec(
-            *command_to_exec,
-            # stdout must a pipe to be accessible as process.stdout
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    # logger.info(e_response)
-    t_response = stdout.decode().strip()
-    if t_response:
-        # logger.info(t_response)
-        x_reponse = t_response
- 
-        if "\n" in x_reponse:
-            x_reponse = x_reponse.split("\n")
- 
-        response_json = json.loads(x_reponse)
-        save_ytdl_json_path = str(msg.from_user.id) + ".json"
- 
-        with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
-            json.dump(response_json, outfile, ensure_ascii=False)
- 
-        fileName = response_json["_filename"]
- 
- 
-    os.remove(save_ytdl_json_path)
-    return fileName
- 
+  command_to_exec = [
+    "youtube-dl", "--no-warnings", "--youtube-skip-dash-manifest", "-j", url
+  ]
+  # logger.info(command_to_exec)
+  process = await asyncio.create_subprocess_exec(
+    *command_to_exec,
+    # stdout must a pipe to be accessible as process.stdout
+    stdout=asyncio.subprocess.PIPE,
+    stderr=asyncio.subprocess.PIPE,
+  )
+  # Wait for the subprocess to finish
+  stdout, stderr = await process.communicate()
+  e_response = stderr.decode().strip()
+  # logger.info(e_response)
+  t_response = stdout.decode().strip()
+  if t_response:
+    # logger.info(t_response)
+    x_reponse = t_response
+
+    if "\n" in x_reponse:
+      x_reponse = x_reponse.split("\n")
+
+    response_json = json.loads(x_reponse)
+    save_ytdl_json_path = str(msg.from_user.id) + ".json"
+
+    with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
+      json.dump(response_json, outfile, ensure_ascii=False)
+
+    fileName = response_json["_filename"]
+
+  os.remove(save_ytdl_json_path)
+  return fileName
+
+
 # getting forrmat details
 async def getting_format(url, msg):
-    command_to_exec = [
-            "youtube-dl",
-            "--no-warnings",
-            "--youtube-skip-dash-manifest",
-            "-j",
-            url
-        ]
-        # logger.info(command_to_exec)
-    process = await asyncio.create_subprocess_exec(
-            *command_to_exec,
-            # stdout must a pipe to be accessible as process.stdout
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    # logger.info(e_response)
-    t_response = stdout.decode().strip()
-    if t_response:
-        # logger.info(t_response)
-        x_reponse = t_response
- 
-        if "\n" in x_reponse:
-            x_reponse = x_reponse.split("\n")
- 
-        response_json = json.loads(x_reponse)
-        save_ytdl_json_path = str(msg.from_user.id) + ".json"
- 
-        with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
-            json.dump(response_json, outfile, ensure_ascii=False)
- 
-        # 
-        if "formats" in response_json:
-            dic = []
-            await msg.reply_text("**Available formats::**", quote=True)
-            for formats in response_json["formats"]:
-                format_id = formats.get("format_id")
-                await msg.reply_text(format_id)
- 
-    os.remove(save_ytdl_json_path)
- 
- 
- 
- 
+  command_to_exec = [
+    "youtube-dl", "--no-warnings", "--youtube-skip-dash-manifest", "-j", url
+  ]
+  # logger.info(command_to_exec)
+  process = await asyncio.create_subprocess_exec(
+    *command_to_exec,
+    # stdout must a pipe to be accessible as process.stdout
+    stdout=asyncio.subprocess.PIPE,
+    stderr=asyncio.subprocess.PIPE,
+  )
+  # Wait for the subprocess to finish
+  stdout, stderr = await process.communicate()
+  e_response = stderr.decode().strip()
+  # logger.info(e_response)
+  t_response = stdout.decode().strip()
+  if t_response:
+    # logger.info(t_response)
+    x_reponse = t_response
+
+    if "\n" in x_reponse:
+      x_reponse = x_reponse.split("\n")
+
+    response_json = json.loads(x_reponse)
+    save_ytdl_json_path = str(msg.from_user.id) + ".json"
+
+    with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
+      json.dump(response_json, outfile, ensure_ascii=False)
+
+    #
+    if "formats" in response_json:
+      dic = []
+      await msg.reply_text("**Available formats::**", quote=True)
+      for formats in response_json["formats"]:
+        format_id = formats.get("format_id")
+        await msg.reply_text(format_id)
+
+  os.remove(save_ytdl_json_path)
+
+
 @app.on_message(filters.private & filters.command('formats'))
-async def getFormat(bot:Client, msg:Message):
-    if (" " in msg.text):
-        cmd, url = msg.text.split(" ", 1)
-    await getting_format(url=url, msg=msg)
- 
+async def getFormat(bot: Client, msg: Message):
+  if (" " in msg.text):
+    cmd, url = msg.text.split(" ", 1)
+  await getting_format(url=url, msg=msg)
+
 
 @app.on_message(filters.private & filters.command('start'))
-async def getFormat(bot:Client, msg:Message):
-    await msg.reply_text(f'**Welcome {msg.from_user.mention}**')
- 
+async def getFormat(bot: Client, msg: Message):
+  await msg.reply_text(f'**Welcome {msg.from_user.mention}**')
+
+
 @app.on_message(filters.incoming | filters.private & filters.text)
-async def downloader(bot:Client, msg:Message):
-    url = msg.text
-    formatt = None
-    if "*" in url:
-        url_parts = url.split("*")
-        if len(url_parts) == 2:
-            url = url_parts[0]
-            formatt = url_parts[1]
-        else:
-            await msg.reply_text("File name error!!", quote=True)
-        if url is not None:
-            url = url.strip()
-        if formatt is not None:
-            formatt = formatt.strip()
-    download_location = await getting_filename(url=url, msg=msg)
-    await download(msg=msg, download_location=download_location, url=url, formatt=formatt)
-    await sender(msg=msg, download_location=download_location, fileName=download_location)
- 
+async def downloader(bot: Client, msg: Message):
+  url = msg.text
+  formatt = None
+  if "*" in url:
+    url_parts = url.split("*")
+    if len(url_parts) == 2:
+      url = url_parts[0]
+      formatt = url_parts[1]
+    else:
+      await msg.reply_text("File name error!!", quote=True)
+    if url is not None:
+      url = url.strip()
+    if formatt is not None:
+      formatt = formatt.strip()
+  download_location = await getting_filename(url=url, msg=msg)
+  print(download_location, url, formatt)
+  await download(msg=msg,
+                 download_location=download_location,
+                 url=url,
+                 formatt=formatt)
+
+
 print('Bot starting!!')
 app.run()
