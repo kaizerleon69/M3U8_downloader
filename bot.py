0cb3215e17885e5c7ae212d465e85b41e7289fad
MalithCrpt <81417838+MalithCrpt@users.noreply.github.com>
2023-05-06 12:59:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,11 @@ from pyrogram import filters, Client
 from pyrogram.types import Message
 import os
 
-if os.path.exists('config.env'):
-  load_dotenv('config.env')
+from config import Config
  
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+bot_token=Config.TG_BOT_TOKEN
+api_id=Config.APP_ID
+api_hash=Config.API_HASH
 
 app = Client("Malith:memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
