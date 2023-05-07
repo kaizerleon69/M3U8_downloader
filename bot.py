0b9c73afece280a38c0989ee4af1bff68b268279
MalithCrpt <81417838+MalithCrpt@users.noreply.github.com>
2023-05-07 10:14:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,20 +15,31 @@ app = Client("Malith:memory:",
 
 @app.on_message(filters.command("start"))
 async def start(_, message):
-    await message.reply_text("Send me an M3U8 URL to download and convert to MP4.")
+    await message.reply_text("Send me an M3U8 URL.")
+    
+@app.on_message(filters.command("download"))
+async def download_m3u8_video(client, message):
+    if len(message.command) < 2:
+        await message.reply("Please provide the m3u8 URL.")
+        return
 
-@app.on_message(filters.text)
-async def download(_, message):
-    url = message.text
-    output = "output.mp4"
+    url = message.command[1]
+    output_file = "output1.mp4"
 
     try:
-        stream = ffmpeg.input(url)
-        stream = ffmpeg.output(stream, output, format="mp4")
-        ffmpeg.run(stream)
-        await message.reply_video(output)
+        await message.reply("Downloading and processing the video...")
+
+        # Download and process the video using ffmpeg-python
+        input_stream = ffmpeg.input(url, err_detect="ignore_err")
+        output_stream = ffmpeg.output(input_stream, output_file, c="copy", bsf_a="aac_adtstoasc")
+        ffmpeg.run(output_stream)
+
+        await message.reply("Uploading the video...")
+        await app.send_video(message.chat.id, output_file)
+        os.remove(output_file)
     except Exception as e:
-        await message.reply_text(f"Error: {e}")
+        await message.reply(f"Error: {e}")
+
 
 
 print('Bot starting!!')
