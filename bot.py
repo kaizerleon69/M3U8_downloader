53a21a625da5257519880708f3ee85d3d9b2a4a7
MalithCrpt <81417838+MalithCrpt@users.noreply.github.com>
2023-05-07 12:57:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-import os
 from pyrogram import Client, filters
-from m3u8_dl import M3U8_DL
-
+from pyrogram.types import Message
+import requests
+import ffmpeg
 
 from config import Config
 
@@ -15,28 +15,38 @@ app = Client("Malith:memory:",
              bot_token=bot_token)
 
 
-def download_m3u8_video(url, output_path):
-    dl = M3U8_DL(url)
-    dl.download()
-    dl.to_mp4(output_path)
+def download_m3u8(url):
+    r = requests.get(url)
+    with open('video.m3u8', 'wb') as f:
+        f.write(r.content)
+
+def convert_m3u8_to_mp4():
+    (
+        ffmpeg
+        .input('video.m3u8')
+        .output('video.mp4', format='mp4')
+        .run()
+    )
+
+def upload_mp4():
+    with open('video.mp4', 'rb') as f:
+        client.send_video(chat_id=<chat_id>, video=f, caption=<caption>)
 
 
     
 @app.on_message(filters.command("start"))
 async def start(_, message):
-    await message.reply_text("Welcome dsfds")
+    await message.reply_text("Welcome")
+    
+@app.on_message(filters.command("convert"))
+async def convert_video(client: Client, message: Message):
+    url = message.text.split(' ')[1]
+    download_m3u8(url)
+    convert_m3u8_to_mp4()
+    upload_mp4()
 
-@app.on_message(filters.command("download") & filters.private)
-def download_video(client, message):
-    url = message.text.split(" ")[1]
-    output_path = "downloaded_video.mp4"
+    await message.reply_text("Video converted and uploaded!")
 
-    message.reply_text("Downloading and converting the video...")
-    try:
-        download_m3u8_video(url, output_path)
-        message.reply_text("Video downloaded and converted successfully.")
-    except Exception as e:
-        message.reply_text(f"Error: {e}")
 
 print('Bot starting!!')
 app.run()
