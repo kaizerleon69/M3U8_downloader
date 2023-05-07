7a9c71ddf648ba28efa33c43e6fb9db96169712e
MalithCrpt <81417838+MalithCrpt@users.noreply.github.com>
2023-05-07 11:32:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,51 +13,36 @@ app = Client("Malith:memory:",
              api_hash=api_hash,
              bot_token=bot_token)
 
-async def download_m3u8(url):
-    command_to_exec = [
-        "youtube-dl",
-        "--no-warnings",
-        "--youtube-skip-dash-manifest",
-        "-j",
-        url,
-    ]
-
-    process = await asyncio.create_subprocess_exec(
-        *command_to_exec,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-
-    if not e_response:
-        video_info = json.loads(t_response)
-        video_url = video_info.get("url")
-        file_name = video_info.get("title")
-        return video_url, file_name
-    else:
-        return None, None
+def download_m3u8(url, download_directory, minus_f_format="best"):
+    command_to_exec = {
+        "format": minus_f_format,
+        "continue_dl": True,
+        "hls_prefer_ffmpeg": True,
+        "embed_subs": True,
+        "outtmpl": download_directory
+    }
+    with youtube_dl.YoutubeDL(command_to_exec) as ydl:
+        ydl.download([url])
 
     
 @app.on_message(filters.command("start"))
 async def start(_, message):
-    await message.reply_text("Welcome to the m3u8 downloader bot! Send me a URL ")
+    await message.reply_text("Welcome ")
 
-@app.on_message(filters.text)
-async def download_and_upload(client, message):
-    url = message.text
-    if "m3u8" in url:
-        video_url, file_name = await download_m3u8(url)
-        if video_url and file_name:
-            await message.reply(f"Downloading {file_name}...")
-            os.system(f'wget -O "{file_name}.mp4" "{video_url}"')
-            await message.reply(f"Uploading {file_name}...")
-            await client.send_video(message.chat.id, f"{file_name}.mp4")
-            os.remove(f"{file_name}.mp4")
-            await message.reply("Upload completed.")
-        else:
-            await message.reply("Error downloading the video.")
+@app.on_message(filters.command(["download"]))
+async def download_handler(client: Client, message: Message):
+    url = message.text.split()[-1]
+    download_directory = "downloads/%(title)s.%(ext)s"
+    minus_f_format = "best"
+
+    await message.reply("Downloading...")
+    download_m3u8(url, download_directory, minus_f_format)
+    await message.reply("Download completed. Uploading...")
+
+    with open(download_directory, 'rb') as f:
+        await client.send_document(chat_id=message.chat.id, document=f)
+
+    await message.reply("Upload completed.")
 
 
 
